3fe81c05042ffce7149f3b4bbfc2bd03b282194d
wjs10 <wjs10@duke.edu>
2025-07-15 21:04:57 -0400

    Add files via upload
    TCAD code files

---
+++ TCAD-Code/sde_dvs.cmd
@@ -0,0 +1,97 @@
+(sde:clear)
+
+(sdegeo:set-default-boolean "ABA")
+
+(define cntlen @<@cntlen@*@xlen@>@ ); contact length
+(define xlen @<@xlen@ + 2*@cntlen@*@xlen@>@)
+
+(define nanowires_len @nanowires_length@)
+(define absorber_length 0.25)
+
+(sdegeo:create-rectangle (position 0 -0.155 0) (position xlen -0.205 0) 
+ "Silver" "Metal_Electrode")
+(sdegeo:create-rectangle (position 0 -0.055 0) (position xlen -0.155 0) 
+ "Aluminum" "Metal_Electrode")
+(sdegeo:create-rectangle (position 0 0 0) (position xlen -0.055 0) 
+ "PCBM" "ETL")
+(sdegeo:create-rectangle (position 0 0 0) (position xlen absorber_length 0) 
+ "Perovskite" "Absorber")
+(sdegeo:create-rectangle (position 0 absorber_length 0) (position xlen  (+ absorber_length 0.005) 0) 
+ "MEO_2PACz" "HTL_buffer")
+(sdegeo:create-rectangle (position 0 (+ absorber_length 0.005) 0) (position xlen  (+ absorber_length 0.054) 0) 
+ "PEDOT" "HTL")
+(sdegeo:create-rectangle (position 0 (+ absorber_length 0.054) 0) (position xlen  (+ absorber_length 0.054 nanowires_len) 0) 
+ "Nanowires_v2" "TCO")
+
+
+;glass or PET layer for substrate
+; (sdegeo:create-cuboid (position xlen ylen  (+ absorber_length 0.124))  (position xlen ylen (+ absorber_length 1.124) ) 
+; "SiO2" "Glass")
+
+
+;contacts
+(sdegeo:define-contact-set "nContact" 4 (color:rgb 1 0 0) "##")
+(sdegeo:define-contact-set "pContact" 4 (color:rgb 0 0 1) "##")
+
+
+(sdegeo:insert-vertex (position cntlen -0.205 0.0))
+(sdegeo:insert-vertex (position (- xlen cntlen) (+ absorber_length 0.054 nanowires_len) 0.0))
+
+(sdegeo:set-current-contact-set "nContact")
+(sdegeo:define-2d-contact (find-edge-id (position (/ cntlen 2) -0.205 0))
+(sdegeo:get-current-contact-set))
+
+(sdegeo:set-current-contact-set "pContact")
+(sdegeo:define-2d-contact (find-edge-id (position (- xlen (/ cntlen 2)) (+ absorber_length 0.054 nanowires_len) 0))
+(sdegeo:get-current-contact-set))
+
+
+
+;meshes
+(define xMax (/ xlen 10) )
+(define xMin (/ xlen 100) )
+
+
+
+
+;Ag and Al mesh
+(sdedr:define-refinement-region "RefPlace.1" "RefDef.1" "Metal_Electrode")
+(sdedr:define-refinement-size "RefDef.1" xMax 0.2 xMin 0.05 )
+
+;pcbm etl mesh
+(sdedr:define-refinement-region "RefPlace.3" "RefDef.3" "ETL")
+(sdedr:define-refinement-size "RefDef.3" xMax 0.02 xMin 0.005 )
+
+;pvsk mesh
+(sdedr:define-refinement-region "RefPlace.4" "RefDef.4" "Absorber")
+(sdedr:define-refinement-size "RefDef.4" xMax 0.1 xMin 0.05 )
+;meo mesh
+(sdedr:define-refinement-region "RefPlace.5" "RefDef.5" "HTL_buffer")
+(sdedr:define-refinement-size "RefDef.5" xMax 0.001 xMin 0.0001 )
+
+;pedot htl mesh
+(sdedr:define-refinement-region "RefPlace.6" "RefDef.6" "HTL")
+(sdedr:define-refinement-size "RefDef.6" xMax  0.05 xMin  0.005 )
+
+;nanowires mesh
+(sdedr:define-refinement-region "RefPlace.7" "RefDef.7" "TCO")
+(sdedr:define-refinement-size "RefDef.7" xMax (/ nanowires_len 4) xMin (/ nanowires_len 10) )
+
+
+;doping
+
+(sdedr:define-constant-profile "PEDOTDoping" "BoronActiveConcentration" 2.5e+21) ;originally 2.5e21
+(sdedr:define-constant-profile-material "PEDOTDopingPlacement" "PEDOTDoping" "PEDOT")
+
+(sdedr:define-constant-profile "nanowireDoping" "BoronActiveConcentration" 2.5e+19)
+(sdedr:define-constant-profile-material "nanowireDopingPlacement" "nanowireDoping" "Nanowires_v2")
+
+(sdedr:define-constant-profile "MEODoping" "BoronActiveConcentration" 9e+18)
+(sdedr:define-constant-profile-material "MEODopingPlacement" "MEODoping" "MEO_2PACz")
+
+(sdedr:define-constant-profile "PcbmDoping" "ArsenicActiveConcentration" 2.2e+18)
+(sdedr:define-constant-profile-material "PcbmDopingPlacement" "PcbmDoping" "PCBM")
+
+(sde:build-mesh " -m 1000000 " "n@node@_msh")
+
+sde:save-model "perovskite_model"
---
+++ TCAD-Code/svisual_vis.tcl
@@ -0,0 +1,115 @@
+#setdep @previous@
+
+
+
+#----------------------------------------------------------------------#
+load_library extract
+lib::SetInfoDef 1
+#----------------------------------------------------------------------#
+
+set n @node@
+# the incidence power from optical sim is sum of intensity in the spectrum file.
+set Psolar [expr 100]	;# Intensity [mW/cm2] of am1.5g spectrum
+set Xlen @xlen@
+
+set cntlen @cntlen@
+set areafactor [expr 1e12/($Xlen*(1+2*$cntlen))]
+echo "af = $areafactor \[cm^2\]"
+load_file n@node|sdevice@_des.plt -name Plt_IV
+
+################################################################
+echo "plotting I-V & P-V curves"
+################################################################
+create_plot -1d -name PlotIV
+select_plots {PlotIV}
+create_curve -name v -dataset Plt_IV -axisX "pContact OuterVoltage" -axisY "pContact OuterVoltage"
+create_curve -name j_orig -dataset Plt_IV -axisX "pContact OuterVoltage" -axisY "nContact TotalCurrent"
+create_curve -name j -function "<j_orig>*$areafactor"
+create_curve -name P -function "<j>*<v>"
+
+set_curve_prop j -label "I-V Curve" -color blue -line_style solid  \
+	-line_width 3 -show_markers -markers_type circle -markers_size 10
+set_curve_prop P -label "P-V Curve" -color red -line_style solid  \
+	-line_width 3 -show_markers -markers_type square -markers_size 10 -axis right
+
+set_axis_prop -axis x -title {Voltage [V]} -title_font_size 16 \
+	-scale_font_size 14 -type linear 
+set_axis_prop -axis y -title {Current Density [mA/cm<sup>2</sup>]} \
+	-title_font_size 16 -scale_font_size 14 -type linear 
+set_axis_prop -axis y2 -title {Power Density [mW/cm<sup>2</sup>]} \
+	-title_font_size 16 -scale_font_size 14 -type linear 
+
+set_plot_prop -title "Light I-V & P-V curves" -title_font_size 20
+set_legend_prop -location bottom_left -font_size 12 -font_att bold
+
+#------------------------------------------------------------
+echo "extracting PV parameters"	
+#------------------------------------------------------------
+#------------------------------------------------------------
+echo "extracting Jsc (mA/cm2) & Voc (V)"	
+#------------------------------------------------------------
+set Js [get_curve_data j -axisY] 
+set Vs [get_curve_data j -axisX] 
+set Ps [get_curve_data P -axisY]
+
+ext::ExtractValue -out Jsc -name "out" -x $Vs -y $Js -xo 0.0 -f "%.4g"
+ext::ExtractValue -out Voc -name "out" -x $Js -y $Vs -xo 0.0 -f "%.4g"
+echo "Jsc=[format %.4g $Jsc] \[mA/cm^2\]" 
+echo "Voc=[format %.4g $Voc] \[V\]"
+
+#------------------------------------------------------------
+echo "extracting Pmpp (mW/cm2), Jmpp (mA/cm2) & Vmpp (V)"	
+#------------------------------------------------------------
+ext::ExtractExtremum -out Pmpp -name "out" 	   -x $Vs -y $Ps -extremum "max" -f "%.4g"
+ext::ExtractValue 	 -out Vmpp -name "out" -x $Ps -y $Vs -xo $Pmpp 	 	 
+ext::ExtractValue 	 -out Jmpp -name "out" -x $Vs -y $Js -xo $Vmpp 	 	 
+echo "Vmpp=[format %.4g $Vmpp] \[V\]"
+echo "Jmpp=[format %.4g $Jmpp] \[mA/cm^2\]"
+echo "Pmpp=[format %.4g $Pmpp] \[mW/cm^2\]"
+
+#------------------------------------------------------------
+echo "extracting Fill Factor (%) and power efficiency (%)"	
+#------------------------------------------------------------
+set FF [expr $Pmpp/($Voc*$Jsc)*100]
+echo "FF=[format %.2f $FF] \%"
+## efficiency in %(mW/cm^2/(100mW/cm^2)*100%)
+set Eff [expr $Pmpp/$Psolar*100]
+echo "Eff=[format %.3f $Eff] \%"
+
+echo "writing values to SWB"
+puts "DOE: FF [format %.2f $FF]"
+puts "DOE: Eff [format %.3f $Eff]"
+
+echo "extracting Rsh (kohm) & Rs (ohm)"	
+	#------------------------------------------------------------
+	echo "js = $Js"
+	set index_rs 0
+	echo "$index_rs"
+	while {[lindex $Js $index_rs] > 0} {
+			incr index_rs	
+	}
+
+	echo "index = $index_rs"
+
+	set dj [expr [lindex $Js $index_rs-1] - [lindex $Js $index_rs]] 
+	set dv [expr [lindex $Vs $index_rs] - [lindex $Vs $index_rs-1]]
+	echo "dj = $dj"
+	echo "dv = $dv"
+
+	set Rs [expr 1000*$dv/$dj]
+	set Rsh [expr [lindex $Vs 1]/($Jsc-[lindex $Js 1])]
+
+	echo "Rsh=[format %.4g $Rsh] \[V/A\]" 
+	echo "Rs=[format %.4g $Rs] \[V/A\]"
+
+	puts "DOE: Rsh [format %.3f $Rsh]"
+	puts "DOE: Rs [format %.3f $Rs]"
+
+
+#------------------------------------------------------------
+
+
+export_curves {j} -plot PlotIV -filename @pwd@/n${n}_iv_original.csv -format csv -overwrite
+remove_curves "v j_orig"
+
+
